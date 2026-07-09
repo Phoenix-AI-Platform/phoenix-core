776798ad33bdce97c05f55b1f3bd0ae71918d29f
Schloss87 <126344020+Schloss87@users.noreply.github.com>
2026-07-09 05:53:03 -0500

    Export Core status payload helpers

--- src/phoenix_core/__init__.py
+++ src/phoenix_core/__init__.py
@@ -8,13 +8,23 @@ from phoenix_core.loader import (
     register_plugins_from_core_config,
 )
 from phoenix_core.plugins import PluginRegistry, RegisteredPlugin
+from phoenix_core.status import (
+    CORE_COMPONENT_NAME,
+    CORE_COMPONENT_VERSION,
+    CORE_STATUS_SCHEMA_VERSION,
+    build_core_status,
+)
 
 __all__ = [
+    "CORE_COMPONENT_NAME",
+    "CORE_COMPONENT_VERSION",
+    "CORE_STATUS_SCHEMA_VERSION",
     "PhoenixCoreConfig",
     "PluginFactoryConfig",
     "PluginLoadError",
     "PluginRegistry",
     "RegisteredPlugin",
+    "build_core_status",
     "load_plugin_factory",
     "register_plugins_from_config",
     "register_plugins_from_core_config",
