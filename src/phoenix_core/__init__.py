5a462d27fdd9d14f8b114f7b87a060915d6016ec
Schloss87 <126344020+Schloss87@users.noreply.github.com>
2026-07-08 21:25:12 -0500

    Export plugin config loader

--- src/phoenix_core/__init__.py
+++ src/phoenix_core/__init__.py
@@ -1,5 +1,12 @@
 """Phoenix Core public API."""
 
+from phoenix_core.loader import PluginLoadError, load_plugin_factory, register_plugins_from_config
 from phoenix_core.plugins import PluginRegistry, RegisteredPlugin
 
-__all__ = ["PluginRegistry", "RegisteredPlugin"]
+__all__ = [
+    "PluginLoadError",
+    "PluginRegistry",
+    "RegisteredPlugin",
+    "load_plugin_factory",
+    "register_plugins_from_config",
+]
